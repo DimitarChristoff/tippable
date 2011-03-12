2522fd1482938af6964a946b35f17f8ae24aaad7
Dimitar Christoff <christoff@gmail.com>
2011-03-12 09:29:35 -0800

    added detatchEvents

--- Source/tippable.js
+++ Source/tippable.js
@@ -3,13 +3,13 @@
 
 name: tippable
 
-description: tippable, a quick fly-in, fly-out tooltip for mootols 1.3
+description: tippable, a quick fly-in, fly-out tooltip
 
 author: Dimitar Christoff
 
 license: MIT-style license.
 
-version: 0.9
+version: 0.95
 
 requires:
   - Core/String
@@ -25,7 +25,7 @@ provides: this.tippable
 this.tippable = new Class({
 
     Implements: [Options, Events],
-
+    
     options: {
         // NB: these are zen classes for Slick
         tipClass: "div.tippable",
@@ -42,27 +42,28 @@ this.tippable = new Class({
             link: "cancel"
         }
     },
-
+    
     initialize: function(element, options) {
         // public instantiation
-
+        
         this.setOptions(options);
         this.element = document.id(element);
         if (!this.element)
             return;
-
+                
         this.attachTip();
     },
-
+    
     attachTip: function() {
         // call creation and events addition
+        
         this.createTip();
         this.attachEvents();
     },
-
+    
     createTip: function() {
         // add tip to dom and set initials
-
+        
         // first event will be onShow
         this.event = "show";
         this.tip = new Element(this.options.tipClass, {
@@ -74,23 +75,23 @@ this.tippable = new Class({
                 this.fireEvent(this.event);
             }.bind(this)
         }));
-
+        
         // store title
         this.title = new Element("div.title").inject(this.tip);
-
+        
         // tip body
         this.body = new Element("div.body").inject(this.tip);
-
+    
         // append to DOM
         this.tip.inject(this.element, "top");
-
+        
         // store instance into the tip element for event use!
         this.element.store("tippable", this);
-
+        
         // set initial title and body values
         this.setTitle(this.options.title);
         this.setHTML(this.options.text);
-
+        
         // now to position element
         if (this.options.leftOffset === false) {
             var tipWidth = this.tip.getSize().x, elWidth = this.element.getSize().x;
@@ -99,7 +100,7 @@ this.tippable = new Class({
         else {
             this.tip.setStyle("marginLeft", this.options.leftOffset);
         }
-
+        
     }, // end createTip
 
     setTitle: function(what) {
@@ -108,52 +109,57 @@ this.tippable = new Class({
             this.title.set("html", what);
         }
     },
-
+     
     setHTML: function(what) {
         // set body content public api
         this.body.set("html", what);
     },
-
+     
     attachEvents: function() {
         // private
         this.element.addEvents({
-            mouseenter: this.showTip.bind(this),
-            mouseleave: this.hideTip.bind(this)
+            mouseenter: this.boundEnter = this.showTip.bind(this),
+            mouseleave: this.boundLeave = this.hideTip.bind(this)
         });
     },
-
+    
+    detatchEvents: function() {
+        this.element.removeEvent("mouseenter", this.boundEnter);
+        this.element.removeEvent("mouseleave", this.boundLeave);
+    },
+    
     showTip: function() {
         // triggered on mousenter or called direct
-
+        
         // fire onBeforeShow
         this.fireEvent("beforeShow");
-
+        
         // next event will be onShow through morph onComplete
         this.event = "show";
-
+        
         // animate properties
         this.tip.morph({
             marginTop: [-this.options.topOffsetStart, -this.options.topOffset],
             opacity: [0, 1]
         });
-
+        
     }, // end showTip
-
+    
     hideTip: function() {
         // triggered on mouseleave ot called direct
-
+        
         // fire onBeforeHide
         this.fireEvent("beforeHide");
-
+        
         // next morph event will be hide
         this.event = "hide";
-
+        
         // hide animation
         this.tip.morph({
             marginTop: -this.options.topOffsetStart,
             opacity: [1, 0]
         });
-
+        
     } // end hideTip
-
+    
 }); // end tippable class
